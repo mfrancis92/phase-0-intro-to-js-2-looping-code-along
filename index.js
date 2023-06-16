af30b9891f9e50979eae06af0a3871839edb00bb
Montell Francis <montelldfrancis@gmail.com>
2023-06-16 02:00:15 -0400

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(num) {
+    let countdown = 0;
+    while (num >= countdown) {
+        console.log(num--);
+    }
+}
+
+countDown(10);
